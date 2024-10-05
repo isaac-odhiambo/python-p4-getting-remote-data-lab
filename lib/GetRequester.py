77d8261689d96162394235f54b31850833b46775
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-10-06 00:39:35 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,16 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """Send a GET request to the URL and return the response body."""
+        response = requests.get(self.url)
+        response.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
+        return response.content
 
     def load_json(self):
-        pass
+        """Use get_response_body to get the response, and convert it to a Python object."""
+        response_body = self.get_response_body()
+        return json.loads(response_body)  # Convert JSON string to Python dictionary/list
